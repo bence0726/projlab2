1da2c6be1fdcc72c2ccbc030931987537f03393f
Bálint <Blint@152.66.176.189>
2016-04-24 18:10:08 +0200

    Test osztály felvétetle és TestManager soronkénti beolvasásra képes.

--- projlab2/src/test/TestManager.java
+++ projlab2/src/test/TestManager.java
@@ -1,5 +1,32 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class TestManager {
+	
+	public static void main(String[] args) {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        ArrayList<String> commandArray = new ArrayList<String>();
+
+         try {
+              String line = null;
+              while (!(line = br.readLine()).equals("")){
+            	  StringTokenizer st = new StringTokenizer(line,",");
+            	  while (st.hasMoreElements()) commandArray.add(st.nextToken()); 
+              }
+         }
+         catch(IOException e){
+        	 e.printStackTrace();
+         }
+         
+         for(String command : commandArray){
+        	 System.out.print(command+"\n");
+         }
+	}
 
 }
