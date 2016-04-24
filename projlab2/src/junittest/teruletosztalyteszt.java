d23e552b0c20285b62346fe344a4beb0ede5dbc6
bence0726 <benze726@gmail.com>
2016-04-24 14:56:30 +0200

    Junit Test Terulet.

---
+++ projlab2/src/junittest/teruletosztalyteszt.java
@@ -0,0 +1,75 @@
+package junittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import projlab2.Terulet;
+import projlab2.Vektor;
+
+public class teruletosztalyteszt {
+
+	@Test
+	public void testIsCoveredBy() {
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Terulet t1 = new Terulet(new Vektor(15,15),new Vektor(25,25));
+		Terulet t2 = new Terulet(new Vektor(15,5),new Vektor(25,15));
+		Terulet t3 = new Terulet(new Vektor(5,15),new Vektor(15,25));
+		Terulet t4 = new Terulet(new Vektor(5,5),new Vektor(15,15));
+		Terulet t5 = new Terulet(new Vektor(100,100),new Vektor(110,110));
+		assertEquals(true, tmain.isCoveredBy(t1));
+		assertEquals(true, tmain.isCoveredBy(t2));
+		assertEquals(true, tmain.isCoveredBy(t3));
+		assertEquals(true, tmain.isCoveredBy(t4));
+		assertEquals(false, tmain.isCoveredBy(t5));
+	}
+	
+	@Test
+	public void testGetDiagonal(){
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Vektor diagonal = tmain.getDiagonal();
+		assertEquals(10.0, diagonal.getVx(),0.001);
+		assertEquals(10.0, diagonal.getVy(),0.001);
+	}
+	@Test
+	public void testGetMiddleOfArea(){
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Vektor middle = tmain.getMiddleOfArea();
+		assertEquals(15.0, middle.getVx(),0.001);
+		assertEquals(15.0, middle.getVy(),0.001);
+	}
+	@Test
+	public void testAddDirToArea(){
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Vektor addvec = new Vektor(5.0,5.0);
+		tmain.addDirToArea(addvec);
+		assertEquals(15.0, tmain.getKezd().getVx(),0.001);
+		assertEquals(15.0, tmain.getKezd().getVy(),0.001);
+		assertEquals(25.0, tmain.getVeg().getVx(),0.001);
+		assertEquals(25.0, tmain.getVeg().getVy(),0.001);
+	}
+	@Test
+	public void testSetNewCornerLocation(){
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Vektor newLoc = new Vektor(100.0,100.0);
+		tmain.setNewCornerLocation(newLoc);
+		assertEquals(100.0, tmain.getKezd().getVx(),0.001);
+		assertEquals(100.0, tmain.getKezd().getVy(),0.001);
+		assertEquals(110.0, tmain.getVeg().getVx(),0.001);
+		assertEquals(110.0, tmain.getVeg().getVy(),0.001);
+	}
+	@Test
+	public void testIsEqualTo() {
+		Terulet tmain = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Terulet tmain2 = new Terulet(new Vektor(10,10),new Vektor(20,20));
+		Terulet tmain3 = new Terulet(new Vektor(12,10),new Vektor(20,20));
+		Terulet tmain4 = new Terulet(new Vektor(10,12),new Vektor(20,20));
+		Terulet tmain5 = new Terulet(new Vektor(10,10),new Vektor(22,20));
+		Terulet tmain6 = new Terulet(new Vektor(10,10),new Vektor(20,22));
+		assertEquals(true, tmain.isEqualTo(tmain2));
+		assertEquals(false, tmain.isEqualTo(tmain3));
+		assertEquals(false, tmain.isEqualTo(tmain4));
+		assertEquals(false, tmain.isEqualTo(tmain5));
+		assertEquals(false, tmain.isEqualTo(tmain6));
+	}
+}
--- projlab2/src/projlab2/Terulet.java
+++ projlab2/src/projlab2/Terulet.java
@@ -63,6 +63,7 @@ public class Terulet {
     		this.kezd.getVy() < t.veg.getVy() &&
     		this.veg.getVy() > t.kezd.getVy() )
     		return true;
+    	
     			
     	return false;
     }
@@ -72,7 +73,7 @@ public class Terulet {
      * Ami a bal felső sarokból mutat a jobb alsóba.
      * @return
      */
-    private Vektor getDiagonal(){
+    public Vektor getDiagonal(){
     	Vektor temp = new Vektor(veg);
     	temp.subVec(kezd);
     	return temp;
