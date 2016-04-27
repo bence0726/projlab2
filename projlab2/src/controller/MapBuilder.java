320d3341bd89237cdca7740ac40fd4de01251556
Zsiga Tibor <tibor.zsiga@hotmail.com>
2016-04-27 13:41:15 +0200

    További reszelések. Létrehoztam egy mapBuilder-t a pálya felépítéséhez.

---
+++ projlab2/src/controller/MapBuilder.java
@@ -0,0 +1,145 @@
+package controller;
+
+import model.Doboz;
+import model.Fal;
+import model.JatekMotor;
+import model.Labirintus;
+import model.Merleg;
+import model.PortalManager;
+import model.Replikator;
+import model.SpecFal;
+import model.Szakadek;
+import model.Terulet;
+import model.Vektor;
+import model.ZPM;
+
+/**
+ * A pálya fájlból való felépítéséhez csináltam ezt az osztályt. 
+ * Az ehhez szükséges függvényeket gyűjti egy csokorba ez az osztály.
+ * @author zsigatibor
+ *
+ */
+public class MapBuilder {
+	/**
+	 * Tárolunk referenciát a Játékmotorra. Tőle fogjuk elkérni
+	 * majd a labirintus és a portalmanager referenciáját,
+	 * és azokat is fogjuk tárolni. De semmiképpen nem itt hozzuk létre.
+	 */
+	private JatekMotor jm;
+	private Labirintus lab;
+	private PortalManager pm;
+	
+	public MapBuilder(JatekMotor gameEngine){
+		jm = gameEngine;
+		lab = jm.getLab();
+		pm = jm.getPortalManager();
+	}
+	
+	/**
+	 * a kapott paraméterekkel meghívja a Fal 
+	 * konstruktorát és hozzáadja a Labirintushoz.
+	 * 
+	 */
+	public void addFal(int x1, int y1, int x2, int y2)
+	{
+		Fal tmp=new Fal(new Terulet(new Vektor(x1,y1), new Vektor(x2,y2)));
+		tmp.setReachable(false);
+		lab.addElem(tmp);
+	}
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a SpecFal konstruktorát
+	 *  és hozzáadja a Labirintushoz.
+	 * 
+	 */
+	public void addSpecFal(int x1, int y1, int x2, int y2)
+	{
+		SpecFal tmp=new SpecFal(new Terulet(new Vektor(x1,y1), new Vektor(x2,y2)), pm);
+		tmp.setReachable(false);
+		lab.addElem(tmp);
+	}
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a ZPM
+	 *  konstruktorát és hozzáadja a Labirintushoz.
+	 */
+	public void addZPM(int x1, int y1, int x2, int y2)
+	{
+		ZPM tmp = new ZPM(new Terulet(new Vektor(x1,y1), new Vektor(x2,y2)));
+		lab.addElem(tmp);
+	}
+	
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a VegeElem konstruktorát
+	 *  és hozzáadja a Labirintushoz.
+	 */
+	public void addVegeElem(int x1, int y1, int x2, int y2)
+	{
+		Fal tmp=new Fal(new Vektor(x1,y1), new Vektor(x2,y2));
+		tmp.setReachable(true);
+		lab.addEndElem(tmp);
+	}
+	
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a Szakadek konstruktorát
+	 *  és hozzáadja a Labirintushoz.
+	 */
+	public void addSzakadek(int x1, int y1, int x2, int y2)
+	{
+		Szakadek tmp = new Szakadek(new Terulet(new Vektor(x1,y1), new Vektor(x2,y2)));
+		lab.addElem(tmp);
+	}
+	
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a Merleg konstruktorát és hozzáadja
+	 * a Labirintushoz. 
+	 * A Merleg konstruktora a kapott paraméterekkel a hozzá tartozó ajtót is létrehozza.
+	 */
+	public void addMerleg(int mx1, int my1, int mx2, int my2, int ax1, int ay1, int ax2, int ay2)
+	{
+		Fal tmp=new Fal(new Terulet(new Vektor(ax1,ay1), new Vektor(ax2,ay2)));
+		tmp.setReachable(false);
+		lab.addElem(new Merleg(new Terulet(new Vektor(mx1,my1), new Vektor(mx2,my2)), tmp));
+	}
+	
+	
+	/**
+	 * a kapott paraméterekkel meghívja a Doboz konstruktorát és hozzáadja a Labirintushoz.
+	 */
+	public void addDoboz(int x, int y, int suly)
+	{
+		Doboz tmp=new Doboz(lab, new Vektor(x,y),suly);
+		lab.addMoveable(tmp);
+	}
+	
+	
+	/**
+	 * a kapott paraméterekkel a Labirintushoz Startelem attribútumát
+	 * állítja be.
+	 */
+	public void addKezdoPont(int x, int y)
+	{
+		Fal tmp=new Fal(new Vektor(x,y), new Vektor(2,2));
+		tmp.setReachable(true);
+		lab.addStartElem(tmp);
+	}
+
+	
+	/**
+	 *a kapott paraméterekkel meghívja a Replikator konstruktorát,
+	 * hozzáadja a Labirintushoz és a mozgatandok listához is.
+	 */
+	public void addReplikator(int x, int y)
+	{
+		Replikator tmp = new Replikator(lab, new Vektor(x,y));
+		lab.addMoveable(tmp);
+	}
+}
---
+++ projlab2/test/test/MainTesterClass.java
@@ -0,0 +1,158 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class MainTesterClass {
+
+	public static void main(String[] args) {
+		try{
+				runTest(args[0]);
+		}catch(IOException e){
+			
+		}
+		
+		
+		try {
+			File fp = new File("src/testfiles",args[0]);
+			BufferedReader brr = new BufferedReader(new FileReader(fp));
+			String line = brr.readLine();
+			while(line != null){
+//				String[] darabok = line.split(" ");
+				System.out.println(TestManager.runCommand(line) + "\n");
+				line = brr.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error while reading file.");
+			e.printStackTrace();
+		}
+		System.out.println("Tesztek lefuttatva.");
+
+	}
+	
+	/**
+	 * az előre megírt testfájl alapján fog futni
+	 * soronként a bemeneti nyelvnek megfelelő parancsokat vár,
+	 * runCommand kapja meg a fájl tartalmát soronként, 
+	 * üres sorig olvas
+	 * 
+	 * erősen béta, a TEST AA bb parancsokkal szeretnénk valamit kezdeni,
+	 * hogy pontosan a bemenet alapján tudjuk, hogy mire akarunk tesztelni
+	 */
+	public static void runTest(String testname)throws IOException{
+		FileInputStream in = null;
+	    FileOutputStream out = null;
+	    BufferedReader reader = null;
+	    String line = null;
+	    String testresult = null;
+	    
+	    try {
+	         in = new FileInputStream("testfiles/"+testname+".txt");
+	         out = new FileOutputStream("testfiles/"+testname+".txt");
+	         reader = new BufferedReader(new InputStreamReader(in));
+	         ArrayList<String> args = new ArrayList<String>();
+	         
+	         line = reader.readLine();
+	         while (line != "TEST"){
+	        	 testresult = TestManager.runCommand(line);
+	        	 line = reader.readLine();
+	        	 if (testresult != null)
+	        	 {
+	        		 byte[] contentInBytes = testresult.getBytes();				
+	        		 out.write(contentInBytes);
+	        		 out.write(System.getProperty("line.separator").getBytes());
+	        		 out.flush();
+				 }
+	        	 
+	         }
+	         while (line != null){
+	        	StringTokenizer st = new StringTokenizer(line," ");
+	     		while (st.hasMoreElements()) args.add(st.nextToken());
+	     		
+	     		switch(args.get(0).toUpperCase()){
+	     			case "TEST":
+	     			if(args.size()==3) {
+	     				switch(args.get(1).toUpperCase()){
+	     				case "DO":
+	     					if(args.get(2)=="ALL"){	//minden meglévő tesztesetet lefuttat
+	 	    				//kérdés: mit takar a minden teszteset? minden testfile a könyvtáron belül,
+	 	    				//vagy egy fájlon belül több dologra akarunk tesztelni? nem igazán látom át
+	 	    				
+	     					}
+	     					else{		//stringet kap, az adott tesztesetet hajtjuk végre
+	 	    				//ugyanaz, mint feljebb, milyen teszteket nézünk?
+	     					}
+	     					break;
+	     				case "MAKE":	//string, teszteset létrehozása
+	 	    				//same problem
+	     					break;
+	     				case "INC":		//string megkeresése az output fájlban/labirintusban whatsthere alapján
+	 	    			
+	     					break;
+	     				case "EXC":		//string keresése INC-hez hasonlóan,
+	 	    						//akkor sikeres a teszt, ha az adott string nem létezik (pl megsemmisült doboz/karakter)	 	    			
+	     					break;
+	 	    		}
+	 	    	}
+	 	    	else
+	 	    		System.out.println("no such command");
+	 	    		
+	 	    break;
+	     		}
+	        	 
+	        	 line = reader.readLine();
+	         }
+	         
+	        //TODO: beolvassa sorra az inputot �sss... valahogy tesztel
+	         /**
+	          * Kimenet mentése ötlet:
+	          * a legtöbb parancs a runCommandon belül stringgel tér vissza, ezeket a
+	          * stringeket a runCommandon belül (vagy akár minden egyes runCmd után az tér vissza stringgel), 
+	          * és akkor a runTest során) mentjük el egy új txt-be, így azonnal tudunk ellenőrizni
+	          */
+	         /*
+	          * Felmerülő kérdések és megoldandó problémák:
+	          * -a switchcase szépen fog pörögni a bemenet alapján, de
+	          * minden egyes kimenetet fájlba kell írni, nem konzolba, különben 
+	          * nem látom, hogy hogyan tudnánk összevetni az elvárt kimenetünkkel, 
+	          * amennyiben nem a consolon akarjuk ellenőrizni, hogy pl MOVED ki mit merre
+	          * ha consoleről olvassuk le, akkor manuálisan látjuk, de a program nem tudja
+	          * összevetni automatikusan, amennyiben így szeretnénk (azt írtuk, hogy valós és elvárt eredmények összehasonlítása)
+	          * 
+	          * -ha bemenet végén definiált értékeket szeretnénk a kimenettel összevetni, amennyiben
+	          * az külön fájlba kerül, akkor a runtest while(line != null) helyett kell egy elválasztó sor,
+	          * és az utána lévő sorokat kell soronként egyeztetni a kimeneti fájlunkkal
+	          * 
+	          * -ha új parancsokat akarunk, melyekket tudjuk kezelni, hogy mit akarunk
+	          * pontosan tesztelni, akkor azoknál is az output fájlt a switchcase folyamán
+	          * kell szépen feltölteni
+	          */
+	         //esetleg �j parancsok felv�tele TEST DO string (1 teszteset futtat�sa)
+	         								//TEST DO ALL (�sszesteset futtat�sa)
+	         								//TEST MAKE string (�j teszteset l�trehoz�sa)
+	         								//TEST INC string (string megl�te az output f�jlban)
+	         								//TEST EXC string (string nem megl�te az output f�jlban)
+	         
+	      }finally {
+	         if (in != null) {
+	            in.close();
+	         }
+	         if (out != null) {
+	            out.close();
+	         }
+	         reader.close();
+	      }
+	   }
+
+}
