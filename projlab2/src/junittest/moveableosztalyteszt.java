7123c918a286fee451f2c15c700177202f8486ef
bence0726 <benze726@gmail.com>
2016-04-28 11:49:53 +0200

    kis javitások

--- projlab2/src/junittest/moveableosztalyteszt.java
+++ projlab2/src/junittest/moveableosztalyteszt.java
@@ -1,18 +1,18 @@
-package junittest;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import model.Karakter;
-import model.Labirintus;
-
-public class moveableosztalyteszt {
-
-//	@Test
-//	public void testKarakterMove() {
-//		Karakter kar = new Karakter(new Labirintus(), new Elem()) 
-//		
-//	}
-
-}
+//package junittest;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//import model.Karakter;
+//import model.Labirintus;
+//
+//public class moveableosztalyteszt {
+//
+////	@Test
+////	public void testKarakterMove() {
+////		Karakter kar = new Karakter(new Labirintus(), new Elem()) 
+////		
+////	}
+//
+//}
