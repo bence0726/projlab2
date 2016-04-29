3c3eb816e6c54c414f284f7e3fe01f34da3bf85a
Bálint <Blint@152.66.176.189>
2016-04-29 09:08:42 +0200

    bat fájl frissítése, így már látja a MainTestClass a txt fájlokat
    ékezetes betűk alkalmazása

--- projlab2/testbuilder.bat
+++ projlab2/testbuilder.bat
@@ -1,8 +1,10 @@
 mkdir build
+mkdir build\test\testfiles
+copy test\testfiles build\test\testfiles
 
-javac ./src/model/*.java -d ./build -encoding utf-8
-javac -cp ./src ./src/controller/*.java -d ./build -encoding utf-8
-javac -cp ./src ./test/test/*.java -d ./build -encoding utf-8
+javac .\src\model\*.java -d .\build -encoding utf-8
+javac -cp .\src .\src\controller\*.java -d .\build -encoding utf-8
+javac -cp .\src .\test\test\*.java -d .\build -encoding utf-8
 
 chcp 65001
 
