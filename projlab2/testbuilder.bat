178f76b53659082be9fe8b8c6318634b1140a9f0
Bálint <Blint@152.66.176.189>
2016-04-29 09:22:30 +0200

    bat fájl javítása chcp 65000

--- projlab2/testbuilder.bat
+++ projlab2/testbuilder.bat
@@ -6,6 +6,6 @@ javac .\src\model\*.java -d .\build -encoding utf-8
 javac -cp .\src .\src\controller\*.java -d .\build -encoding utf-8
 javac -cp .\src .\test\test\*.java -d .\build -encoding utf-8
 
-chcp 65001
+chcp 65000
 
 cd build
