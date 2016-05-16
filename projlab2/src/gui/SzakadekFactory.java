0d340dbc461169d81841d94e9e32c4f316ccfb38
bence0726 <benze726@gmail.com>
2016-05-16 14:25:10 +0200

    kesz

--- projlab2/src/gui/SzakadekFactory.java
+++ projlab2/src/gui/SzakadekFactory.java
@@ -33,11 +33,7 @@ public class SzakadekFactory implements Factory{
 		panel.setBounds((int)e.getPos().getKezd().getVx(), (int)e.getPos().getKezd().getVy(), (int)e.getPos().getWidth(), (int)e.getPos().getHeight());
 		ImageIcon imageIcon = new ImageIcon("src/textures/Szakadek.jpg"); // load the image to a imageIcon
 		Image image = imageIcon.getImage(); // transform it 
-<<<<<<< HEAD
 		Image newimg = image.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-=======
-		Image newimg = image.getScaledInstance(40,40, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
->>>>>>> branch 'master' of https://github.com/bence0726/projlab2.git
 		imageIcon = new ImageIcon(newimg);  // transform it back
 		for (int i=0;i<rows*columns;i++)
 		{
